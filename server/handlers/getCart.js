51b6e73afe8b2f81bfb0ae7a66bb4beb95db8700
MTLFibbs <119969967+MTLFibbs@users.noreply.github.com>
2023-03-22 15:59:02 -0400

    added getCart endpoint

--- server/handlers/getCart.js
+++ server/handlers/getCart.js
@@ -12,8 +12,30 @@ const options = {
 
 const { v4: uuidv4 } = require("uuid");
 
+// get the list of all items in the cart of the specific user, in this case the hardcoded JimmyBuyMore@realcustomer.ca
 const getCart = async (req,res) => {
+  const client = new MongoClient(MONGO_URI,options);
+  const {useremail} = req.params;
+  const query = useremail;
 
+  try{
+    await client.connect();
+    const db = client.db("GroupProject");
+    const result = await db.collection("Cart").find({userEmail: query}).toArray()
+
+
+    if(result.length === 0){
+      res.status(500).json({ status: 500, data: query, message: "This user does not currently have a cart" });
+    }
+    else{
+      res.status(200).json({ status: 200, data: result, message: `Cart list for user ${query} found!` });
+    }
+
+  }catch(err){
+    console.log(err.stack);
+    res.status(500).json({ status: 500, data: req.body, message: err.message });
+  }
+  client.close();
 };
 
 module.exports = {getCart};
